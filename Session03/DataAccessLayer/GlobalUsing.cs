f5154b900e82942df299509632bc02d1ffbbc85d
MohamedSabryy <mhmdasabry@gmail.com>
2024-09-18 14:37:49 +0300

    video 2

---
+++ Session03/DataAccessLayer/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using Session03.DataAccessLayer.Models;
+global using System.ComponentModel.DataAnnotations;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Identity;
