025fd41335b1201e00a964053878005abca0220a
MohamedSabryy <mhmdasabry@gmail.com>
2024-09-18 14:56:39 +0300

    video 3

---
+++ Session03/Session03.presentationLayer/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Session03.presentationLayer.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
