8c9778bf8b7aa3c15de0c4df6e381683b63b90bc
MohamedSabryy <mhmdasabry@gmail.com>
2024-09-25 23:01:15 +0300

    video 3

--- Session03/Session03.presentationLayer/Controllers/UsersController.cs
+++ Session03/Session03.presentationLayer/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Session03.presentationLayer.Controllers
 {
@@ -42,5 +43,68 @@ namespace Session03.presentationLayer.Controllers
 			};
 			return View(model);
 		}
-	}
+
+		public async Task<IActionResult> Details(string id, string ViewName=nameof(Details))
+		{
+            if (string.IsNullOrWhiteSpace( id)) return BadRequest();
+            var user = await _userManager.FindByIdAsync( id);
+            if (user is null) return NotFound();
+			var userModel = new UserViewModel
+			{ 
+				Email = user.Email,
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				Id = user.Id,
+				UserName = user.UserName,
+				Roles = await _userManager.GetRolesAsync(user)
+			};
+			return View(ViewName, userModel);
+
+        }
+
+		public async Task<IActionResult> Edit(string id) => await Details(id, nameof(Edit));
+
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(string id, UserViewModel model)
+		{
+			if(id != model.Id) return BadRequest();
+			if (!ModelState.IsValid) return View(model);
+			try
+			{
+				var user = await _userManager.FindByEmailAsync(model.Email);	
+				if (user is null) return NotFound();
+				user.FirstName = model.FirstName;
+				user.LastName = model.LastName;
+				await _userManager.UpdateAsync(user);
+				return RedirectToAction(nameof(Index));
+			}
+			catch (Exception ex) 
+			{
+				ModelState.AddModelError(string.Empty, ex.Message);
+			}
+			return View(model);
+		}
+        public async Task<IActionResult> Delete(string id) => await Details(id, nameof(Delete));
+
+		[HttpPost, ActionName("Delete")]
+		public async Task<IActionResult> ConfirmDelete(string id)
+		{
+            try
+            {
+                var user = await _userManager.FindByIdAsync(id);
+                if (user is null) return NotFound();
+            
+                await _userManager.DeleteAsync(user);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+			return View();
+        }
+
+    }
 }
+
